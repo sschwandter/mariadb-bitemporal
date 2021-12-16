13ea085a8690e25af4c90d65a1d2a1999e5ec70f
Stefan Schwandter <stefan.schwandter@xion.at>
2021-12-16 16:53:31 +0100

    Initial commit.

---
+++ src/main/java/at/xion/mariadbbitemporal/MariadbBitemporalApplication.java
@@ -0,0 +1,13 @@
+package at.xion.mariadbbitemporal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariadbBitemporalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariadbBitemporalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/xion/mariadbbitemporal/MariadbBitemporalApplicationTests.java
@@ -0,0 +1,13 @@
+package at.xion.mariadbbitemporal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariadbBitemporalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
